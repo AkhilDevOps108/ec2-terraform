24811e334cb060e286d1f0c322ab41c8d0e29022
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-07-10 20:20:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,26 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS Region to deploy in"
+  type        = string
+}
+
 variable "ami_id" {
-  default = "ami-03bb6d83c60fc5f7c"  # Amazon Linux 2 in ap-south-1
+  description = "AMI ID to use for EC2"
+  type        = string
+  default     = "ami-03bb6d83c60fc5f7c" # Amazon Linux 2 in ap-south-1
 }
 
 variable "instance_type" {
-  default = "t2.micro"  # Free-tier eligible
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
